b30da29437eeaa3ed7ee82c3871b66b027524830
ivn <ivn@li.ru>
2019-12-12 23:51:48 +0300

    start billing project

---
+++ project/mysql/cmd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+/usr/bin/mysqld_safe &
+sleep 10s
+echo "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY 'changeme' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
+mysql -uroot < project/mysql/create_radius_db.sql && mysql -uroot radius < project/mysql/schema.sql && mysql -uroot radius < project/mysql/setup.sql
+echo "GRANT ALL PRIVILEGES ON *.* To 'root'@'%' IDENTIFIED BY 'passwd';" | mysql
+killall mysqld
+sleep 10s
+/usr/bin/mysqld_safe
